9d25cf1515d9da24807b8e6bbb749bb3c721e7e4
chris-greening <chrisgreening3@gmail.com>
2020-11-15 23:20:32 -0500

    add: added get_daily_infections_data

--- update_numbers.py
+++ update_numbers.py
@@ -1,8 +1,11 @@
 import os 
 import datetime
+import json
+import re
 
 import requests
 import pandas as pd
+from bs4 import BeautifulSoup
 import tweepy 
 
 CONSUMER_KEY = os.environ.get("COVID19_TWITTER_CONSUMER_KEY")
@@ -36,6 +39,27 @@ def construct_tweet(data):
     )
     return tweet_str
 
+def get_daily_infections_data(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text)
+    script_tags = soup.find_all('script')
+    outer_break = False
+    for script in script_tags:
+        for c in script.contents:
+            if 'Daily New Cases' in c:
+                outer_break = True
+                break
+        if outer_break:
+            break
+    matches = re.findall('\[(.+?)\]', c)
+    date = matches[0].split(',')
+    new_infections = matches[1].split(',')
+    data = list(zip(date, new_infections))
+    data = [d for d in data if d[1] != 'null']
+    data = [(d[0], int(d[1])) for d in data]
+    df = pd.DataFrame(data)
+    print(df)
+
 def tweet_daily_numbers(api, tweet_str):
     api.update_status(tweet_str)
 
@@ -44,5 +68,6 @@ if __name__ == '__main__':
 
     api = return_api()
     data = scrape_worldometers_data(URL)
-    tweet_str = construct_tweet(data)
-    tweet_daily_numbers(api, tweet_str)
+    # tweet_str = construct_tweet(data)
+    # tweet_daily_numbers(api, tweet_str)
+    get_daily_infections_data(URL)
