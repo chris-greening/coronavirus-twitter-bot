69543be497d8a65ac3c86e8b90dffa09ad1ea688
chris-greening <chrisgreening3@gmail.com>
2020-11-15 21:35:40 -0500

    add: added scrape and tweet functionality

--- update_numbers.py
+++ update_numbers.py
@@ -1,5 +1,8 @@
 import os 
+import datetime
 
+import requests
+import pandas as pd
 import tweepy 
 
 CONSUMER_KEY = os.environ.get("COVID19_TWITTER_CONSUMER_KEY")
@@ -7,9 +10,39 @@ CONSUMER_SECRET = os.environ.get("COVID19_TWITTER_CONSUMER_SECRET")
 ACCESS_TOKEN = os.environ.get("COVID19_TWITTER_ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = os.environ.get("COVID19_TWITTER_ACCESS_TOKEN_SECRET")
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+def return_api():
+    """Return the API"""
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth)
+    return api
 
-api = tweepy.API(auth)
+def scrape_worldometers_data(url):
+    """Return scraped Worldometers data"""
+    r = requests.get(url)
+    tables = pd.read_html(r.text)
+    data = tables[0]
+    return data.iloc[0]
 
-api.update_status('Welcome to Coronavirus News!\n\nThis is a volunteer, semi-autonomous Twitter page dedicated to centralizing and providing accurate information regarding the ongoing COVID19 health crisis.\n\nMessage me here or @ChrisGreening2 if you have any questions.')
+def construct_tweet(data):
+    todays_date = datetime.datetime.now().strftime("%A, %m/%d/%Y")
+    tweet_str = (
+        f"United States - Daily Update\n"
+        f"{todays_date}\n\n"
+        f"🤢 Total Infections: {data.TotalCases:,} ({data.NewCases:,})\n"
+        f"⚰️ Total Deaths....: {data.TotalDeaths:,} ({data.NewDeaths:,})\n\n"
+        f"Source: https://www.worldometers.info/coronavirus/country/us/\n"
+        f"Daily update tweeted everyday @8PM EST"
+    )
+    return tweet_str
+
+def tweet_daily_numbers(api, tweet_str):
+    api.update_status(tweet_str)
+
+if __name__ == '__main__':
+    URL = "https://www.worldometers.info/coronavirus/country/us/"
+
+    api = return_api()
+    data = scrape_worldometers_data(URL)
+    tweet_str = construct_tweet(data)
+    tweet_daily_numbers(api, tweet_str)
