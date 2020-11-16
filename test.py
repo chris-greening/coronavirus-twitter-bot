4fc3f59787c93eba6278f8cb8addd5bba1c1adce
chris-greening <chrisgreening3@gmail.com>
2020-11-15 19:27:06 -0500

    add: created test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+import os 
+
+import tweepy 
+
+CONSUMER_KEY = os.environ.get("COVID19_TWITTER_CONSUMER_KEY")
+CONSUMER_SECRET = 
+ACCESS_TOKEN = 
+ACCESS_TOKEN_SECRET = 
+
+auth = tweepy.OAuthHandler()
