406e8b732e12c7abd389f591ca76d5990c17a13d
chris-greening <chrisgreening3@gmail.com>
2020-11-17 21:22:12 -0500

    add: added _build_task_queue method

--- covid_bot.py
+++ covid_bot.py
@@ -1,9 +1,11 @@
 import datetime
 import os
+from collections import deque
 
 import tweepy
 
-from auto_tweet import AutoTweet
+from scheduled_task_registry import SCHEDULED_TASK_REGISTRY
+from scheduled_task import ScheduledTask
 
 class CovidBot:
     """
@@ -16,9 +18,15 @@ class CovidBot:
     ACCESS_TOKEN_SECRET = os.environ.get("COVID19_TWITTER_ACCESS_TOKEN_SECRET")
 
     def __init__(self):
-        self._connect_twitter_api()
+        self.scheduled_tasks = SCHEDULED_TASK_REGISTRY
         self.dt = datetime.datetime.now()
-        AutoTweet.connect_bot_api(self.api)
+        self._connect_twitter_api()
+        ScheduledTask.connect_bot_api(self.api)
+        self._build_task_queue(self.dt, self.scheduled_tasks)
+
+    def _build_task_queue(self, dt, scheduled_tasks):
+        """Get a queue of all tasks that are to be performed this run"""
+        self.task_queue = deque([task for task in scheduled_tasks if task.is_scheduled_to_run(dt)])
 
     def _connect_twitter_api(self):
         """Connect bot to the Twitter API"""
