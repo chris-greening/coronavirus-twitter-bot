2b29019c76f4041934b26eec3c58d347afae4027
chris-greening <chrisgreening3@gmail.com>
2020-11-17 21:06:49 -0500

    add: added attach_image and attach_text methods

--- tweet.py
+++ tweet.py
@@ -1,3 +1,11 @@
 class Tweet:
     def __init__(self):
-        pass
+        self.image_filepath = None
+        self.tweet_text = None
+
+    def attach_image(self, fpath: str):
+        """Attach an image to the tweet"""
+        self.image_filepath = fpath
+
+    def attach_text(self, tweet_text: str):
+        self.tweet_text = tweet_text
