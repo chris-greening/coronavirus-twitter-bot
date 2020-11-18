86c66e494bdfc4bf1adce7b9aede6ef60db63fcc
chris-greening <chrisgreening3@gmail.com>
2020-11-17 19:39:32 -0500

    add: finished compare_datetime method

--- auto_tweet.py
+++ auto_tweet.py
@@ -13,9 +13,17 @@ class AutoTweet:
         self.hour = hour
         self.end_of_month = end_of_month
 
-    def compare_datetime(self, other_datetime):
+    def compare_datetime(self, today_datetime):
         """Returns True if other_datetime matches the internal scheduled time"""
-        pass
+        today_weekday = today_datetime.strftime("%A")
+        today_time = today_datetime.time()
+        today_hour = self._round_hour(today_time)
+
+        return True if today_datetime in self.weekday and today_hour in self.hour else False
+
+    def _round_hour(self, time):
+        """Return rounded hour"""
+        return time.hour + 1 if time.minute // 30 ==  1 else time.hour
 
     @classmethod
     def connect_bot_api(cls, api):
