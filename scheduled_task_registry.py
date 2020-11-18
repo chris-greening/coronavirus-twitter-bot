60b0df8321ed8d55235747517243cf147af4f9be
chris-greening <chrisgreening3@gmail.com>
2020-11-18 00:27:46 -0500

    docs: added module docstring

--- scheduled_task_registry.py
+++ scheduled_task_registry.py
@@ -1,9 +1,19 @@
+"""
+Registry of all the scheduled tasks to be performed by the bot
+"""
+
 from scheduled_task import ScheduledTask
 from update_numbers import get_daily_update
+from weekly_graphs import get_daily_infection_plot
 
 SCHEDULED_TASK_REGISTRY = [
     ScheduledTask(
         task_function=get_daily_update,
         hours=[22]
+    ),
+    ScheduledTask(
+        task_function=get_daily_infection_plot,
+        weekdays=['Sunday'],
+        hours=[9]
     )
 ]
