ed90a93d542a39988290009388bc30caf3ad6f39
chris-greening <chrisgreening3@gmail.com>
2020-11-17 21:54:29 -0500

    refactor: changed tuple to list

--- scheduled_task_registry.py
+++ scheduled_task_registry.py
@@ -1,9 +1,9 @@
 from scheduled_task import ScheduledTask
 from update_numbers import get_daily_update
 
-SCHEDULED_TASK_REGISTRY = (
+SCHEDULED_TASK_REGISTRY = [
     ScheduledTask(
         task_function=get_daily_update,
-        hours=(20)
+        hours=[22]
     )
-)
+]
