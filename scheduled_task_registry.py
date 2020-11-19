21b99f298885e8bfea4c388d7c7c9b134e5881ca
chris-greening <chrisgreening3@gmail.com>
2020-11-19 15:18:51 -0500

    modify: changed times

--- scheduled_task_registry.py
+++ scheduled_task_registry.py
@@ -9,11 +9,11 @@ from weekly_graphs import get_daily_infection_plot
 SCHEDULED_TASK_REGISTRY = [
     ScheduledTask(
         task_function=get_daily_update,
-        hours=[22]
+        hours=[20]
     ),
     ScheduledTask(
         task_function=get_daily_infection_plot,
         weekdays=["Sunday"],
-        hours=[9]
+        hours=[21]
     )
 ]
