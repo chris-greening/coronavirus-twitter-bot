9f9179bbccef17b0230cce8e4cc0ebaa8fd2c8e3
chris-greening <chrisgreening3@gmail.com>
2020-11-20 19:38:22 -0500

    add: added comments to describe the two tasks

--- scheduled_task_registry.py
+++ scheduled_task_registry.py
@@ -7,10 +7,12 @@ from update_numbers import get_daily_update
 from weekly_graphs import get_daily_infection_plot
 
 SCHEDULED_TASK_REGISTRY = [
+    #Scrape daily infections/death data from Worldometers and tweet update
     ScheduledTask(
         task_function=get_daily_update,
         hours=[20]
     ),
+    #Scrape graph from Worldometers and tweet the graph
     ScheduledTask(
         task_function=get_daily_infection_plot,
         weekdays=["Wednesday", "Sunday"],
